b02321cb295490a4b8135f34b77922ffe24e49ac
Cornélio Petersen Júnior <corneliopj@gmail.com>
2025-08-25 17:58:11 +0000

    atualizacao

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
-require_once 'config/config.php';
-require_once 'controllers/dashboard.php';
-require_once 'includes/header.php';
-require_once 'includes/sidebar.php';
-require_once 'views/dashboard.php';
-require_once 'includes/footer.php';
+require_once '../config/config.php';
+require_once '../controllers/dashboard.php';
+require_once '../includes/header.php';
+require_once '../includes/sidebar.php';
+require_once '../views/dashboard.php';
+require_once '../includes/footer.php';
 ?>
